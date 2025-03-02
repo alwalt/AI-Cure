6e95ec2937db7fe8584378083f3a9d4e6a1d60ac
Erick Watanabe <erick.j.watanabe@nasa.gov>
2025-03-02 12:02:36 -0800

    Add color transitions and hover effects to UploadFile button

--- src/components/base/UploadFileButton.tsx
+++ src/components/base/UploadFileButton.tsx
@@ -34,11 +34,13 @@ export default function UploadFileButton({
   };
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center relative group">
       <button onClick={() => setIsOpen(true)} className="text-white rounded">
-        <ArrowUpTrayIcon className="h-6 w-6" />
+        <ArrowUpTrayIcon className="h-6 w-6 stroke-primaryWhite stroke-1 text-primaryBlack hover:stroke-redFill transition-colors duration-300" />
       </button>
-
+      <span className="absolute top-full mt-1 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-primaryBlack border-primaryWhite border-1 text-xs text-white opacity-0 transition-opacity group-hover:opacity-100 px-2 py-1">
+        Upload file
+      </span>
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-50" onClose={setIsOpen}>
           <TransitionChild
