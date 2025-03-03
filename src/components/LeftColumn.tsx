0a09c34db216d928d9e8fe076cf3ac27ae732e8f
Erick Watanabe <erick.j.watanabe@nasa.gov>
2025-03-03 14:53:34 -0800

    Create CollectionManager and saveButton components for Left Column

--- src/components/LeftColumn.tsx
+++ src/components/LeftColumn.tsx
@@ -1,13 +1,15 @@
 import FilesManager from "./FilesManager";
+import CollectionMangaer from "./CollectionManager";
 
 interface LeftColumnProps {
-	onPreview: (csvFilename: string, sessionId: string) => void;
+  onPreview: (csvFilename: string, sessionId: string) => void;
 }
 
 export default function LeftColumn({ onPreview }: LeftColumnProps) {
-	return (
-		<div className="bg-primaryBlack border-blue-500 border-2 p-4 h-full">
-			<FilesManager onPreview={onPreview} />
-		</div>
-	);
+  return (
+    <div className="bg-primaryBlack border-blue-500 border-2 p-4 h-full space-y-8">
+      <FilesManager onPreview={onPreview} />
+      <CollectionMangaer />
+    </div>
+  );
 }
