eb40140111ed35fb174b96082edf2ac0f17ad806
Harlan <harlanphillips74@gmail.com>
2025-03-10 16:48:19 -0400

    feat(FileUploader): Ability to handle multiple files

---
+++ src/types/files.ts
@@ -0,0 +1,18 @@
+export interface UploadedFile {
+  name: string;
+  type: string;
+  dateCreated: string;
+  size: number;
+  file: File;
+  selected?: boolean;
+}
+
+export interface Table {
+  csv_filename: string;
+  display_name: string;
+}
+
+export interface UploadResponse {
+  session_id: string;
+  tables: Table[];
+} 
