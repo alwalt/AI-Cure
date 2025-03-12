55f61f01e3db669803eb916e55b4dbadf6568c89
Erick Watanabe <erick.j.watanabe@nasa.gov>
2025-03-12 14:22:48 -0700

    Refactor AicureToolFull removed unused lines

--- src/components/AicureToolFull.tsx
+++ src/components/AicureToolFull.tsx
@@ -4,39 +4,15 @@ import LeftColumn from "./LeftColumn";
 import RightColumn from "./RightColumn";
 import MiddleTopColumn from "./MiddleTopColumn";
 import MiddleBottomColumn from "./MiddleBottomColumn";
-import { useSessionFileStore } from "@/store/useSessionFileStore";
-import FilesManager from "./FilesManager";
-import { UploadedFile } from "@/types/files";
 
 export default function AicureToolFull() {
   const [showRight, setShowRight] = useState(true);
-
-  const sessionId = useSessionFileStore((state) => state.sessionId);
-  const previewCsv = useSessionFileStore((state) => state.previewCsv);
-  const previewFile = useSessionFileStore((state) => state.previewFile);
-  const setSessionId = useSessionFileStore((state) => state.setSessionId);
-  const setPreviewCsv = useSessionFileStore((state) => state.setPreviewCsv);
-  const setPreviewFile = useSessionFileStore((state) => state.setPreviewFile);
-
   const toggleRightColumn = () => setShowRight((prev) => !prev);
-
-  const handlePreview = (csvFilename: string, currentSessionId: string) => {
-    setPreviewCsv(csvFilename);
-    setSessionId(currentSessionId);
-    setPreviewFile(null); // Clear any file preview when showing CSV
-  };
-
-  const handleFilePreview = (file: UploadedFile | null) => {
-    setPreviewFile(file);
-  };
 
   return (
     <div className="flex h-screen">
       <div className="w-1/4">
-        <LeftColumn
-          onPreview={handlePreview}
-          onFilePreview={handleFilePreview}
-        />
+        <LeftColumn />
       </div>
 
       <div className="flex flex-col h-screen flex-grow">
