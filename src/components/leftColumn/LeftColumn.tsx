d38b35b7ec30e3acd8b8c56d515d100f44a34893
Harlan <harlanphillips74@gmail.com>
2025-05-20 18:33:21 -0400

    feat(fileManagement): Implement 'Add to Collection' functionality and refine UI
    - Add selected files from Files area to Collections area
    - Integrate 'Add to Collection' button within UploadedFiles component
    - Remove redundant selected file count from FilesManager
    - Suppress alert popup after adding files to collection
    - Ensure CollectionsManager displays files added to collection
    - Prevent duplicate file entries in collections

--- src/components/leftColumn/LeftColumn.tsx
+++ src/components/leftColumn/LeftColumn.tsx
@@ -1,11 +1,12 @@
-import FilesManager from "./filesArea/FilesManager";
-import CollectionMangaer from "./collectionsArea/CollectionManager";
+import FilesManager from "./FilesArea/FilesManager";
+import CollectionManager from "./CollectionsArea/CollectionManager";
 
 export default function LeftColumn() {
+  console.log("LeftColumn component is rendering");
   return (
     <div className="bg-primaryBlack border-r-2 border-gray-700 p-2 h-full space-y-8">
       <FilesManager />
-      <CollectionMangaer />
+      <CollectionManager />
     </div>
   );
 }
