45c1bb170f5df469609b88e2673b930371a0aeb4
Erick Watanabe <erick.j.watanabe@nasa.gov>
2025-07-15 13:58:23 -0700

    Add magic wand for ai generate button from lucide-react and tooltipId

--- src/components/base/AiGenerateButton.tsx
+++ src/components/base/AiGenerateButton.tsx
@@ -1,5 +1,5 @@
 import Button from "./Button";
-import { SparklesIcon } from "@heroicons/react/24/outline";
+import { Wand } from "lucide-react";
 
 interface AiGenerateButtonProps {
   onClick?: () => Promise<void> | void;
@@ -9,15 +9,16 @@ interface AiGenerateButtonProps {
 export default function AiGenerateButton({ onClick }: AiGenerateButtonProps) {
   return (
     <Button
-      targetId="FolderPlusButton"
+      targetId="Ai-generate-button"
       buttonDescription="AI generate"
-      Icon={SparklesIcon}
+      Icon={Wand}
       iconClassName="h-8 w-8 p-1 border border-primaryWhite rounded-lg hover:border-redFill hover:border"
       onClick={onClick} //pass the async function
       aria-label="Generate content with AI" // Accessible label for screen readers
       role="button" // Explicitly defines the role as a button (this is usually implied for <button> elements)
       className="focus:outline-none focus:ring-2 focus:ring-primaryWhite" // Focus ring for keyboard navigation
       spanClassName="static ml-10 -translate-y-8"
+      tooltipId="tooltip-ai-generate"
     />
   );
 }
