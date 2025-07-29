90293e7f69ae77512372960cb473c4b70fcda886
Erick Watanabe <erick.j.watanabe@nasa.gov>
2025-07-29 11:08:35 -0700

    Add Assays tab, delete pdfExtractor and PowerPointTab

---
+++ src/components/middleColumns/middleTopCol/Assays.tsx
@@ -0,0 +1,7 @@
+export default function Assays() {
+  return (
+    <div className="w-full overflow-auto">
+      <div className="rounded overflow-hidden border border-grey"></div>
+    </div>
+  );
+}
